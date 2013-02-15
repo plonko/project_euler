0f89caf4fd090d56f42537b848ea50c928a98147
seb <seb@u-dox.com>
2013-02-15 15:04:36 +0000

    Made it into a function

--- problem3.py
+++ problem3.py
@@ -4,14 +4,14 @@ What is the largest prime factor of the number 600851475143 ?
 """
 
 from math import sqrt
-a=[]
-pf=13195
-pflimit=int(sqrt(pf))
-for n in xrange(2,pflimit):
-    if pf%n==0:
-        for x in xrange(2,n):
-            if n%x == 0:
-                break
-        else:
-            pf=pf/n
-        print('pf now = ' +str(n))
+
+def largest_factor(pf):
+    pflimit=int(sqrt(pf))
+    for n in xrange(2,pflimit):
+        if pf%n==0:
+            for x in xrange(2,n):
+                if n%x == 0:
+                    break
+            else:
+                pf=pf/n
+            print('pf now = ' +str(n))
